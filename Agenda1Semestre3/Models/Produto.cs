2b83f44cafc1c8bb9d2385d983fa7396cab5df91
Brunogaldinoarruda@Gmail.com <Brunogaldinoarruda@Gmail.com>
2025-09-07 21:46:28 -0300

    07/09/2025 Agenda 4

--- Agenda1Semestre3/Models/Produto.cs
+++ Agenda1Semestre3/Models/Produto.cs
@@ -9,5 +9,7 @@ namespace Agenda1Semestre3.Models
         public string Descricao { get; set; }
         public double Quantidade {  get; set; }
         public double Preco {  get; set; }
+
+        public double Total { get => Quantidade * Preco; }
     }
 }
--- Agenda1Semestre3/Views/ListaProduto.xaml.cs
+++ Agenda1Semestre3/Views/ListaProduto.xaml.cs
@@ -1,11 +1,26 @@
+using Agenda1Semestre3.Models;
+using System.Collections.ObjectModel;
+
 namespace Agenda1Semestre3.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	// Usado para deixa a lista atualizando automaticamente 
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
+	// Sempre é chamado quando uma tela aparece e está em outro processo
+    protected async override void OnAppearing()
+    {
+		//Criando a lista de produtos aguardavel 
+		List<Produto> tmp = await App.Db.GetAll();
+		// Colhendo as informações da lista generica para uma lista de objetos/ForEach
+		tmp.ForEach( i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -17,5 +32,31 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+	
+		//usado para limpar a lista
+		lista.Clear();
+
+        //Criando a lista de preucura aguardavel 
+        List<Produto> tmp = await App.Db.Search(q);
+        // Colhendo as informações da lista generica para uma lista de objetos/ForEach
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+		DisplayAlert("Total de produtos", msg, "Ok");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
