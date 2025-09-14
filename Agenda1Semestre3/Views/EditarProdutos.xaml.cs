938eec3ea64989db8c563ace05e260b054bc156d
Brunogaldinoarruda@Gmail.com <Brunogaldinoarruda@Gmail.com>
2025-09-14 16:11:13 -0300

    14/09/2025 CRUD finalizado

--- Agenda1Semestre3/Views/EditarProdutos.xaml.cs
+++ Agenda1Semestre3/Views/EditarProdutos.xaml.cs
@@ -1,3 +1,5 @@
+using Agenda1Semestre3.Models;
+
 namespace Agenda1Semestre3.Views;
 
 public partial class EditarProdutos : ContentPage
@@ -6,4 +8,28 @@ public partial class EditarProdutos : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Registro Atualizados", "Ok");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    
+    }
 }
--- Agenda1Semestre3/Views/ListaProduto.xaml.cs
+++ Agenda1Semestre3/Views/ListaProduto.xaml.cs
@@ -1,5 +1,6 @@
 using Agenda1Semestre3.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Agenda1Semestre3.Views;
 
@@ -16,10 +17,17 @@ public partial class ListaProduto : ContentPage
 	// Sempre é chamado quando uma tela aparece e está em outro processo
     protected async override void OnAppearing()
     {
-		//Criando a lista de produtos aguardavel 
-		List<Produto> tmp = await App.Db.GetAll();
-		// Colhendo as informações da lista generica para uma lista de objetos/ForEach
-		tmp.ForEach( i => lista.Add(i));
+		try
+		{
+			//Criando a lista de produtos aguardavel 
+			List<Produto> tmp = await App.Db.GetAll();
+			// Colhendo as informações da lista generica para uma lista de objetos/ForEach
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -36,27 +44,67 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
-	
-		//usado para limpar a lista
-		lista.Clear();
+		try
+		{
+			string q = e.NewTextValue;
 
-        //Criando a lista de preucura aguardavel 
-        List<Produto> tmp = await App.Db.Search(q);
-        // Colhendo as informações da lista generica para uma lista de objetos/ForEach
-        tmp.ForEach(i => lista.Add(i));
+			//usado para limpar a lista
+			lista.Clear();
+
+			//Criando a lista de preucura aguardavel 
+			List<Produto> tmp = await App.Db.Search(q);
+			// Colhendo as informações da lista generica para uma lista de objetos/ForEach
+			tmp.ForEach(i => lista.Add(i));
+		}
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 
-    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
-		double soma = lista.Sum(i => i.Total);
+		try
+		{
+			double soma = lista.Sum(i => i.Total);
 
-		string msg = $"O total é {soma:C}";
-		DisplayAlert("Total de produtos", msg, "Ok");
+			string msg = $"O total é {soma:C}";
+			await DisplayAlert("Total de produtos", msg, "Ok");
+		}
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private async void MenuItem_Clicked(object sender, EventArgs e)
     {
+		MenuItem selecinado = sender as MenuItem;
+		Produto p = selecinado.BindingContext as Produto;
+		bool confirm = await DisplayAlert("Tem Certeza?", $"Remover {p.Descricao}", "Sim", "Não");
 
+		if (confirm)
+		{
+			await App.Db.Delete(p.Id);
+			lista.Remove(p);
+		}
+
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		try
+		{
+			Produto p = e.SelectedItem as Produto;
+
+			Navigation.PushAsync(new Views.EditarProdutos
+			{
+				BindingContext = p,
+			});
+		}
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 }
